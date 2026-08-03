e0ae386dfb76c412fcd37f65ecd2cc584cb9f15f
CyberL1 <cyber.hf18@gmail.com>
2026-08-03 20:45:15 +0200

    fix win build script

--- build_win.sh
+++ build_win.sh
@@ -8,9 +8,9 @@ ORIGINAL_DATA_PATH=$2
 MODDED_DATA_PATH=$3
 XDELTA_FILE_PATH=$4
 
-if test -z $CLI_PATH || test -z $ORIGINAL_DATA_PATH || test -z $MODDED_DATA_PATH || test -z $XDELTA_FILE_PATH; then
+if test -z $CLI_PATH || test -z $ORIGINAL_DATA_PATH || test -z $XDELTA_FILE_PATH || test -z $MODDED_DATA_PATH; then
   echo "Usage: $0 path/to/xdelta path/to/original_data.win path/to/file.xdelta path/to/modded_data.win"
   exit 1
 fi
 
-$CLI_PATH -e -s $ORIGINAL_DATA_PATH $MODDED_DATA_PATH $XDELTA_FILE_PATH
+$CLI_PATH -ds $ORIGINAL_DATA_PATH $MODDED_DATA_PATH $XDELTA_FILE_PATH
