1a7449a33942bcf4aadc5a1fc970ec4f03f2470d
CyberL1 <cyber.hf18@gmail.com>
2026-08-03 20:37:52 +0200

    add save location option to patch.sh script

--- patch.sh
+++ patch.sh
@@ -4,11 +4,12 @@
 # Usage: ./patch.sh path/to/UndertaleModToolCli path/to/data.win
 
 CLI_PATH=$1
-DATA_PATH=$2
+ORIGINAL_DATA_PATH=$2
+MODDED_DATA_PATH=$3
 
-if test -z $CLI_PATH || test -z $DATA_PATH; then
-  echo "Usage: $0 path/to/UndertaleModToolCli path/to/data.win" 
+if test -z $CLI_PATH || test -z $ORIGINAL_DATA_PATH || test -z $MODDED_DATA_PATH; then
+  echo "Usage: $0 path/to/UndertaleModToolCli path/to/data.win path/to/modded.win" 
   exit 1
 fi
 
-$CLI_PATH load $DATA_PATH -o $DATA_PATH -s VesselNameInsteadOfKris.csx
+$CLI_PATH load $ORIGINAL_DATA_PATH -o $MODDED_DATA_PATH -s VesselNameInsteadOfKris.csx
