90a2ee65dd352e31579f5f63b66d49aabce5a5ec
CyberL1 <cyber.hf18@gmail.com>
2026-08-03 20:45:28 +0200

    add patch build script

---
+++ builld_patch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Require: xdelta (https://github.com/jmacd/xdelta/releases)
+# Usage: ./build_patch.sh path/to/xdelta path/to/original_data.win path/to/modded_data.win path/to/file.xdelta
+
+CLI_PATH=$1
+ORIGINAL_DATA_PATH=$2
+MODDED_DATA_PATH=$3
+XDELTA_FILE_PATH=$4
+
+if test -z $CLI_PATH || test -z $ORIGINAL_DATA_PATH || test -z $MODDED_DATA_PATH || test -z $XDELTA_FILE_PATH; then
+  echo "Usage: $0 path/to/xdelta path/to/original_data.win path/to/modded_data.win path/to/file.xdelta"
+  exit 1
+fi
+
+$CLI_PATH -es $ORIGINAL_DATA_PATH $MODDED_DATA_PATH $XDELTA_FILE_PATH
