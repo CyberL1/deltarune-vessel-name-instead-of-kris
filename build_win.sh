bd9f59214889e5beaeef97d37d84357f05a2c85a
CyberL1 <cyber.hf18@gmail.com>
2026-08-03 21:09:27 +0200

    move patch.sh to build_patch.sh

--- build_win.sh
+++ build_win.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-# Require: xdelta (https://github.com/jmacd/xdelta/releases)
-# Usage: ./build_win.sh path/to/xdelta path/to/original_data.win path/to/file.xdelta path/to/modded_data.win
+# Require: UndertaleModToolCli (https://github.com/UnderminersTeam/UndertaleModTool/releases)
+# Usage: ./build_win.sh path/to/UndertaleModToolCli path/to/original_data.win path/to/modded_data.win
 
 CLI_PATH=$1
 ORIGINAL_DATA_PATH=$2
 MODDED_DATA_PATH=$3
-XDELTA_FILE_PATH=$4
 
-if test -z $CLI_PATH || test -z $ORIGINAL_DATA_PATH || test -z $XDELTA_FILE_PATH || test -z $MODDED_DATA_PATH; then
-  echo "Usage: $0 path/to/xdelta path/to/original_data.win path/to/file.xdelta path/to/modded_data.win"
+if test -z $CLI_PATH || test -z $ORIGINAL_DATA_PATH || test -z $MODDED_DATA_PATH; then
+  echo "Usage: $0 path/to/UndertaleModToolCli path/to/data.win path/to/modded.win" 
   exit 1
 fi
 
-$CLI_PATH -ds $ORIGINAL_DATA_PATH $MODDED_DATA_PATH $XDELTA_FILE_PATH
+$CLI_PATH load $ORIGINAL_DATA_PATH -o $MODDED_DATA_PATH -s VesselNameInsteadOfKris.csx
